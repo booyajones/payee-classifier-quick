21562feccd2256633e5e793cc146c4ff7e1a9a9a
booyajones <chris.a.wyatt@gmail.com>
2025-06-23 15:41:03 -0400

    chore: remove deprecated classification exports

--- src/lib/classificationEngine.ts
+++ src/lib/classificationEngine.ts
@@ -5,7 +5,6 @@
 export * from './classification';
 export { enhancedClassifyPayee } from './classification/enhancedClassification';
 export { enhancedProcessBatch } from './classification/enhancedBatchProcessor';
-export { consensusClassification } from './openai/enhancedClassification';
 export { getConfidenceLevel, classifyPayee } from './classification/utils';
 export { DEFAULT_CLASSIFICATION_CONFIG } from './classification/config';
 export { processBatch } from './classification/batchProcessing';
@@ -19,11 +18,6 @@ export {
   bulkKeywordExclusion 
 } from './classification/enhancedKeywordExclusion';
 
-export * from './openai/batchAPI';
-
-// Export new true batch API functionality
-export * from './openai/trueBatchAPI';
-export * from './openai/hybridBatchProcessor';
 
 // Export enhanced V2 functions
 export { enhancedClassifyPayeeV2 } from './classification/enhancedClassificationV2';
